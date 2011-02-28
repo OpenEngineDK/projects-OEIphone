cc0b9ccee70fc95fa4119d8b4f8d7ac8deefe738
Asger Dam Hoedt <asgerhoedt@gmail.com>
2011-02-28 14:38:32 +0000

    Depending on iOSEnvironment and makeapp.sh works

--- makeapp.sh
+++ makeapp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mkdir iOERacer.app
+mkdir -p iOERacer.app
 cp projects/OEIphone/repo-data/* iOERacer.app/
-cp projects/OEIphone/data/* iOERacer.app/
+cp -r projects/OEIphone/data/* iOERacer.app/
 cp build/OEIphone/OEIphone iOERacer.app/iOERacer
